8cffe1dd09b663e63f54c98a14a4001d130eac24
Dylan Gunn <dylgunn@wl.k12.ia.us>
2018-11-16 15:03:42 -0600

    Created new favroite teachers file

---
+++ js/teachers.js
@@ -0,0 +1,27 @@
+var teachers = [
+    {
+        image: "gunn.jpg",
+        name : "Mike Gunn", 
+        subjects: ["Science","physics"],
+        teacher_id:"1001",
+    },
+    {
+        image: "iske.jpg",
+        name: "Jason Iske", 
+        subjects: ["Ingenuity"],
+        teacher_id:"1002",
+    },
+    {    
+        image: "nelson.jpg",
+        name : "Robert Nelson", 
+        subjects: ["English","Public speacking"],
+        teacher_id:"1003",
+    },
+    {
+        image: "diemer.jpg",
+        name : "Kurt Diemer", 
+        subjects: ["History","Sociology"],
+        teacher_id:"1005", 
+    },
+    ];
+    
